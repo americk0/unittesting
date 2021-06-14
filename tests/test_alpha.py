8b76d2503220b6e54f24f733bc6d1bf3b4c82183
Ben Watson <ben.watson.developer@gmail.com>
2021-06-14 16:49:38 -0400

    second commit

--- tests/test_alpha.py
+++ tests/test_alpha.py
@@ -2,6 +2,6 @@ import pytest
 import unittesting.alpha
 
 def test_alpha(mocker):
-    mocker.patch('mymodule.alpha.bravo', return_value=2)
+    mocker.patch('unittesting.alpha.bravo', return_value=2)
 
     assert get_alpha() == 2
